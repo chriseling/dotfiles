145fb3c6aa3f9a455a1af8c7355916dfbcc734b8
vshen <victor.shen@berkeley.edu>
2015-06-08 11:10:55 -0700

    Added tiling manager and specific app hints

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -1,4 +1,5 @@
 local alert = require "mjolnir.alert"
+local appfinder = require "mjolnir.cmsj.appfinder"
 local application = require "mjolnir.application"
 local fnutils = require "mjolnir.fnutils"
 local grid = require "mjolnir.bg.grid"
@@ -6,6 +7,7 @@ local hints = require "mjolnir.th.hints"
 local hotkey = require "mjolnir.hotkey"
 local modal_hotkey = require("mjolnir._asm.modal_hotkey").inject()
 local screen = require "mjolnir.screen"
+local tiling = require "mjolnir.tiling"
 local window = require "mjolnir.window"
 
 local mash = {"cmd", "ctrl"}
@@ -58,4 +60,23 @@ hotkey.bind(mash, "Right", function()
   win:setframe(f)
 end)
 
+-- Window hint / specific program focusing
 hotkey.bind(mash, "q", hints.windowHints)
+
+hotkey.bind(mash, "c", function()
+  hints.appHints(appfinder.app_from_name("Google Chrome"))
+end)
+
+hotkey.bind(mash, "t", function()
+  hints.appHints(appfinder.app_from_name("Terminal"))
+end)
+
+hotkey.bind(mash, "m", function()
+  hints.appHints(appfinder.app_from_name("Messages"))
+end)
+
+-- Tiling layouts
+hotkey.bind(mash, "0", function() tiling.cyclelayout() end)
+hotkey.bind(mash, "1", function() tiling.cycle(1) end)
+hotkey.bind(mash, "2", function() tiling.cycle(-1) end)
+hotkey.bind(mash, "space", function() tiling.promote() end)
