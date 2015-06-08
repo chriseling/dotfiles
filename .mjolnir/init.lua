537b1d2023b6e5f1577c380cde35c933dcf2352b
vshen <victor.shen@berkeley.edu>
2015-06-08 10:25:49 -0700

    fixed key bindings to use one global leader instead of hardcoding cmd ctrl everywhere

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -8,30 +8,31 @@ local modal_hotkey = require("mjolnir._asm.modal_hotkey").inject()
 local screen = require "mjolnir.screen"
 local window = require "mjolnir.window"
 
-hotkey.bind({"cmd", "ctrl"}, "R", function()
+local mash = {"cmd", "ctrl"}
+hotkey.bind(mash, "R", function()
   mjolnir.reload()
 end)
 
-hotkey.bind({"cmd", "ctrl"}, "H", function()
+hotkey.bind(mash, "H", function()
   local win = window.focusedwindow()
   local f = win:frame()
   f.h = f.h / 2
   win:setframe(f)
 end)
 
-hotkey.bind({"cmd", "ctrl"}, "V", function()
+hotkey.bind(mash, "V", function()
   local win = window.focusedwindow()
   local f = win:frame()
   f.w = f.w / 2
   win:setframe(f)
 end)
 
-hotkey.bind({"cmd", "ctrl"}, "Up", function()
+hotkey.bind(mash, "Up", function()
   local win = window.focusedwindow()
   win:maximize()
 end)
 
-hotkey.bind({"cmd", "ctrl"}, "Left", function()
+hotkey.bind(mash, "Left", function()
   local win = window.focusedwindow()
   local f = win:frame()
   local primary_screen = screen.mainscreen()
@@ -44,7 +45,7 @@ hotkey.bind({"cmd", "ctrl"}, "Left", function()
   win:setframe(f)
 end)
 
-hotkey.bind({"cmd", "ctrl"}, "Right", function()
+hotkey.bind(mash, "Right", function()
   local win = window.focusedwindow()
   local f = win:frame()
   local primary_screen = screen.mainscreen()
@@ -57,4 +58,4 @@ hotkey.bind({"cmd", "ctrl"}, "Right", function()
   win:setframe(f)
 end)
 
-hotkey.bind({"cmd", "ctrl"}, "q", hints.windowHints)
+hotkey.bind(mash, "q", hints.windowHints)
